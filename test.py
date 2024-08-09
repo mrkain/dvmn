d0057152caa7461e2558251d0d6cdad978bb3c44
mrkain <leks.popow@gmail.com>
2024-08-10 01:08:51 +0300

    1 lesson

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-1
+import requests
+
+
+article_ids = ["Лондон", "Череповец", "Шереметьево", "fggwg"]
+url_template = "https://wttr.in/{}?M?nTqu&lang=ru"
+
+for article_id in article_ids:
+    url = url_template.format(article_id)
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Ошибка {response.status_code}")
